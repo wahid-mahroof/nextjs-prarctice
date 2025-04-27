cf0b8962b58b3d3faf9274dcccdb286d166a8233
wahid-mahroof <wahidmahroof040@gmail.com>
2025-04-27 11:51:41 -0700

    first commit

---
+++ app/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function () {
+  return (
+    <div>
+      Sign In
+      <input type="text"></input>
+      <input type="text"></input>
+      <button>Sign In </button>
+    </div>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function () {
+  return (
+    <div>
+      SignUp
+      <input type="text"></input>
+      <input type="text"></input>
+      <button>Sign Up</button>
+    </div>
+  );
+}
