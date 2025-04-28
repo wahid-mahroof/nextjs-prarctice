47491af62f5b38029f161398f62b9f2b0746c274
wahid-mahroof <wahidmahroof040@gmail.com>
2025-04-28 01:55:10 -0700

    local storage next js

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import axios from "axios";
+
 export default function () {
   return (
     <div>
       Sign In
       <input type="text"></input>
       <input type="text"></input>
-      <button>Sign In </button>
+      <button
+        onClick={async () => {
+          const res = await axios.post("http://localhost:3000/api/signin", {
+            username: "wahid",
+            password: "123123",
+          });
+          localStorage.setItem("token", res.data.token);
+        }}
+      >
+        Sign In{" "}
+      </button>
     </div>
   );
 }
